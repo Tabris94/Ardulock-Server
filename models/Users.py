8a8b0b7ac6a0cc7dca561e6722310c7c7b01bb9f
Your Name <andrea.cipollaro.94@gmail.com>
2018-12-28 17:01:16 +0100

    Fix Model

--- models/Users.py
+++ models/Users.py
@@ -6,7 +6,7 @@ from uuid import uuid4
 
 class Users(Document):
     email = StringField(required = True, unique = True)
-    irst_name = StringField(max_lenght = 20, required = True)
+    first_name = StringField(max_lenght = 20, required = True)
     last_name = StringField(max_lenght = 20, required = True)
     password = StringField(min_lengt = 6, required = True)
     token = StringField(max_lenght = 20, unique = True)
